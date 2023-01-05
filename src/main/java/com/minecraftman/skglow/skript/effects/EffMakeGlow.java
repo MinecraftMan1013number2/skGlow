6f4640610f642eedc8a8dcf4e7655f4fbe0f43a9
MinecraftMan1013number2 <70504198+MinecraftMan1013number2@users.noreply.github.com>
2023-01-04 22:36:19 -0500

    Changed from SkriptColor to EGlowColor

--- src/main/java/com/minecraftman/skglow/skript/effects/EffMakeGlow.java
+++ src/main/java/com/minecraftman/skglow/skript/effects/EffMakeGlow.java
@@ -5,10 +5,8 @@ import ch.njol.skript.doc.*;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
-import ch.njol.skript.util.SkriptColor;
 import ch.njol.util.Kleenean;
 import com.minecraftman.skglow.SkGlow;
-import com.minecraftman.skglow.utils.ColorTypes;
 import me.MrGraycat.eGlow.API.EGlowAPI;
 import me.MrGraycat.eGlow.API.Enum.EGlowColor;
 import me.MrGraycat.eGlow.EGlow;
@@ -17,28 +15,32 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
-
 @Name("Make Glow")
 @Description("Make a player glow a color")
-@Examples({"make player glow red", "make all players glow blue"})
+@Examples({"make player glow red glow", "make all players glow blue glow"})
 @Since("1.0.0")
 @RequiredPlugins("eGlow")
 public class EffMakeGlow extends Effect {
 	
 	static {
-		Skript.registerEffect(EffMakeGlow.class, "make %players% glow %color%");
+		// TODO:
+		//  possibly make
+		Skript.registerEffect(EffMakeGlow.class,
+			"make %players% glow %glowcolor%",
+			"apply %glowcolor% to %players%"
+		);
 	}
 	
 	private Expression<Player> players;
-	private Expression<SkriptColor> color;
+	private Expression<EGlowColor> color;
+//	private Expression<SkriptColor> color;
 	private final EGlowAPI api = EGlow.getAPI();
 	
 	@SuppressWarnings({"unchecked", "NullableProblems"})
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
 		this.players = (Expression<Player>) exprs[0];
-		this.color = (Expression<SkriptColor>) exprs[1];
+		this.color = (Expression<EGlowColor>) exprs[1];
 		return true;
 	}
 	
@@ -50,7 +52,8 @@ public class EffMakeGlow extends Effect {
 		System.out.println(color.toString());
 		System.out.println(color.getSingle(e));
 		
-		EGlowColor glowColor = ColorTypes.toEGlowColor(Objects.requireNonNull(color.getSingle(e)));
+//		EGlowColor glowColor = ColorTypes.toEGlowColor(color.getSingle(e));
+		EGlowColor glowColor = color.getSingle(e);
 		if (glowColor == null) {
 			SkGlow.getPluginLogger().info("Color '" + color.getSingle(e) + "' is invalid!");
 			return;
