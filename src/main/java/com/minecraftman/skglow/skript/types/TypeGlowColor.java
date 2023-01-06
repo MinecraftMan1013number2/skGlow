1031b63640547078671bc76b4814a7ddc35e988e
MinecraftMan1013number2 <70504198+MinecraftMan1013number2@users.noreply.github.com>
2023-01-05 22:20:03 -0500

    Added BlinkSpeed type

--- src/main/java/com/minecraftman/skglow/skript/types/TypeGlowColor.java
+++ src/main/java/com/minecraftman/skglow/skript/types/TypeGlowColor.java
@@ -46,5 +46,39 @@ public class TypeGlowColor {
 				}
 			})
 		);
+		
+		Classes.registerClass(new ClassInfo<>(BlinkSpeed.class, "blinkspeed")
+			.user("blink ?speeds?")
+			.name("Blink Speed")
+			.description("Represents a blink speed (fast or slow)")
+			.usage("fast", "slow")
+			.examples("")
+			.since("2.0.0")
+			.parser(new Parser<BlinkSpeed>() {
+				@SuppressWarnings("NullableProblems")
+				@Override
+				public BlinkSpeed parse(String input, ParseContext context) {
+					input = input.toUpperCase();
+					if (!input.equals("FAST") && !input.equals("SLOW")) {
+						Skript.error("Invalid speed (" + input + ")! (Expected 'fast' or 'slow')");
+						return null;
+					} else {
+						return BlinkSpeed.valueOf(input);
+					}
+				}
+				
+				@SuppressWarnings("NullableProblems")
+				@Override
+				public String toVariableNameString(BlinkSpeed speed) {
+					return speed.name().toLowerCase();
+				}
+				
+				@SuppressWarnings("NullableProblems")
+				@Override
+				public String toString(BlinkSpeed speed, int flags) {
+					return toVariableNameString(speed);
+				}
+			})
+		);
 	}
 }
