cf42ffa5c69db364ee1d6b32e74a10ca9a79484d
MinecraftMan1013number2 <70504198+MinecraftMan1013number2@users.noreply.github.com>
2023-01-16 11:31:32 -0500

    Update annotations

--- src/main/java/com/minecraftman/skglow/skript/effects/EffBlink.java
+++ src/main/java/com/minecraftman/skglow/skript/effects/EffBlink.java
@@ -1,6 +1,7 @@
 package com.minecraftman.skglow.skript.effects;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.*;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
@@ -14,6 +15,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
+@Name("Make Blink")
+@Description("Make a player blink a color")
+@Examples({"make player blink purple slowly", "apply slow red blink to player"})
+@Since("2.0.0")
+@RequiredPlugins("eGlow")
 public class EffBlink extends Effect {
 	static {
 		Skript.registerEffect(EffBlink.class,
